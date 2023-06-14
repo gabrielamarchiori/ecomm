c2959cea9cb4688f7f9f5f96ada55d4b4c6376a9
Marchiori Gabriela Cristina Amancio <n135417@gruposantander.com>
2023-06-14 13:57:37 -0300

    fix: insercaoCategoria function

--- src/cli/CategoryService.js
+++ src/cli/CategoryService.js
@@ -1,40 +1,98 @@
 import chalk from "chalk";
 
 async function findCategories() {
-    const response = await fetch("http://localhost:3000/categories", 
-    { method: 'GET'})
-    .then(response => response.json())
-    .catch(error => console.log('error', error));
-    console.log(`Response status: ${chalk.yellow(200)}` ,response)
-}
-async function findCategoryById(id) {
-    const response = await fetch(`http://localhost:3000/categories/${id}`, 
-    { method: 'GET'})
-    .then(response => response.json())
-    .catch(error => console.log('error', error));
-    if(Object.keys(response).length === 0) {
-        console.log(`Response status: ${chalk.yellow(404)}` ,'Essa categoria não existe')
-    } else {
+    try {
+        const response = await fetch("http://localhost:3000/categories", 
+        { method: 'GET'})
+        
+        const status = response.status
+        const responseData = await response.json()
 
-        console.log(`Response status: ${chalk.yellow(200)}` ,response)
+        console.log(`response status: ${chalk.yellow(status)}`, responseData)
+    }
+    catch (error) {
+        console.log(error)
     }
 }
-async function createCategory () {
-    const response = await fetch(`http://localhost:3000/categories`, 
-    { 
-        method: 'POST',
-        headers: {'Content-Type' : 'application/json'},
-        body: {nome: 'VESTUÁRIO', status: 'ATIVA'},
-    })
-    .then(response => JSON.parse(response))
-    .catch(error => console.log('error', error));
-    
-    console.log(`Response status: ${chalk.yellow(200)}` ,response)
-    
+
+async function findCategoryById(id) {
+    try {
+        const response = await fetch(`http://localhost:3000/categories/${id}`, 
+        { method: 'GET'})
+        
+        const status = response.status
+        const responseData = await response.json()
+
+        if(Object.keys(responseData).length == 0) {
+            console.log(`Response status: ${chalk.yellow(status)}` ,'Essa categoria não existe')
+        } 
+        else {
+            console.log(`Response status: ${chalk.yellow(status)}` ,responseData)
+        }
+    }
+    catch (error) {
+        console.log(error)
+    }
 }
 
-const categoryService = {findCategories, findCategoryById, createCategory}
+async function createCategory () {
+    try {
+        const response = await fetch(`http://localhost:3000/categories`, 
+        { 
+            method: 'POST',
+            headers: {'Content-Type' : 'application/json'},
+            body: JSON.stringify({nome: 'VESTUÁRIO', status: 'ATIVA'}),
+        })
+        
+        const status = response.status
+        const responseData = await response.json()
+                
+        console.log(`Response status: ${chalk.yellow(status)}`, responseData)
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
 
-console.log(await categoryService.createCategory())
+async function updateCategory (id) {
+    try {
+        const response = await fetch(`http://localhost:3000/categories/${id}`, 
+        { 
+            method: 'PATCH',
+            headers: {'Content-Type' : 'application/json'},
+            body: JSON.stringify({nome: 'AUTOMÓVEIS'}),
+
+        })
+        
+        const status = response.status
+        const responseData = await response.json()
+
+        console.log(`Response status: ${chalk.yellow(status)}` ,`response data:`, responseData)
+        
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+async function deleteCategory (id) {
+    try {
+        const response = await fetch(`http://localhost:3000/categories/${id}`, 
+        { method: 'DELETE'})
+        
+        const status = response.status
+        const responseData = await response.json()
+
+        console.log(`Response status: ${chalk.yellow(status)}` ,responseData)
+        
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+const categoryService = {findCategories, findCategoryById, createCategory, updateCategory, deleteCategory}
+
+
 
 export default categoryService
--- src/cli/cli.js
+++ src/cli/cli.js
@@ -1,4 +1,6 @@
+import process from 'node:process'
 import categoryService from "./CategoryService.js"
+import fs from 'fs'
 
 const caminho = process.argv;
 console.log(caminho)
@@ -16,8 +18,20 @@ async function processarComandos(args) {
         categoryService.findCategoryById(id)
         .catch(error => console.log('erro', error))
     }
+    
     if(caminho === '--inserirCategoria'){
-         categoryService.createCategory()
+        const file = args[3]
+        fs.writeFileSync(file, await categoryService.createCategory() )
+        .catch(error => console.log('erro', error))
+    }
+    if(caminho === '--atualizarCategoria'){
+        const id = args[3]
+        categoryService.updateCategory(id)
+        .catch(error => console.log('erro', error))
+    }
+    if(caminho === '--excluirCategoria'){
+        const id = args[3]
+        categoryService.deleteCategory(id)
         .catch(error => console.log('erro', error))
     }
     
