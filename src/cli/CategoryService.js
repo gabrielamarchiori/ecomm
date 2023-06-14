254121221756a37b3f496a9915d7efe5edebf59c
Marchiori Gabriela Cristina Amancio <n135417@gruposantander.com>
2023-06-14 10:06:03 -0300

    add: categoryService

--- src/cli/CategoryService.js
+++ src/cli/CategoryService.js
@@ -1,1 +1,23 @@
+import chalk from "chalk";
+
 console.log('to funcionando')
+
+
+async function findCategories() {
+    const response = await fetch("http://localhost:3000/categories", 
+    { method: 'GET'})
+    .then(response => response.json())
+    .catch(error => console.log('error', error));
+    console.log(`Response status: ${chalk.yellow(200)}` ,response)
+}
+async function findCategoryById(id) {
+    const response = await fetch(`http://localhost:3000/categories/${id}`, 
+    { method: 'GET'})
+    .then(response => response.json())
+    .catch(error => console.log('error', error));
+    console.log(`Response status: ${chalk.yellow(200)}` ,response)
+}
+
+const categoryService = {findCategories, findCategoryById}
+
+export default categoryService
--- src/cli/cli.js
+++ src/cli/cli.js
@@ -1,55 +1,28 @@
-import chalk from 'chalk';
-import fs from 'fs'
+import categoryService from "./CategoryService.js"
 
-function trataErro(erro) {
-    console.log(erro)
-    throw new Error(erro.code, 'Não há arquivo no diretório')
+const caminho = process.argv;
+
+async function processarComandos(args) {
+    const caminho = args[2];
+    
+    switch (caminho) {
+        case "--listarCategorias":
+            try {
+                await categoryService.findCategories();
+            } catch (error) {
+                console.log("erro:", error);
+            }
+            break;
+    }
+    switch (caminho) {
+        case "----recuperarCategoriaPorId":
+            try {
+                await categoryService.findCategoryById();
+            } catch (error) {
+                console.log("erro:", error);
+            }
+            break;
+    }
 }
 
-function imprimeLista(resultado, identificador='') {
-    console.log(
-        `Response status:${chalk.yellow('200')}`,
-        chalk.green(resultado)
-    )
-}
-
-async function pegaDb(caminhoDoArquivo) {
-    try {
-        const encoding = 'utf-8';
-        const dataBase = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        return dataBase
-    }
-    catch (erro) {
-        trataErro(erro)
-    }
-}
-const caminho = process.argv
-
-async function processarComando(argumentos) {
-    const caminho = argumentos[2]
-
-    try{
-        fs.lstat(caminho)
-    } catch (erro) {
-        if(erro.code === 'ENOENT') {
-            console.log('arquivo ou diretório não existe')
-            return
-        }
-    }
-
-    if(fs.lstatSync(caminho).isFile()) {
-        const resultado = await pegaDb(argumentos[2])
-        imprimeLista(resultado)
-    } else if (fs.lstatSync(caminho).isDirectory()) {
-        const arquivos = await fs.promises.readdir(caminho)
-        arquivos.forEach(async (nomeDoArquivo) => {
-            const lista = await pegaDb(`${caminho}/${nomeDoArquivo}`)
-            imprimeLista(lista, nomeDoArquivo)
-        })
-    }
-
-
-}
-
-// console.log(await pegaDb('./src/cli/db.json'))
-processarComando(caminho)
+processarComandos(caminho);
